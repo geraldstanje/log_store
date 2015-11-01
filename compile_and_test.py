21116bd64a1f8b512cb9c942eb9233331a4f5cdb
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-11-01 22:47:50 +0100

    added unit test and valgrind check script

---
+++ compile_and_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import os
+from struct import *
+import subprocess
+import shlex
+
+def run_sub_process(mylist):
+  pipe = subprocess.Popen(mylist, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  stdout, stderr = pipe.communicate()
+  stdout_test = stdout.decode('utf-8')
+  stderr_res = stderr.decode('utf-8')
+  return stdout, stderr_res
+
+def main():
+  # compile unit test
+  run_sub_process(['rm', '*.log'])
+  run_sub_process(['rm', '*.data'])
+  run_sub_process(['make', 'clean'])
+  run_sub_process(['make', 'BUILD=test'])
+
+  # run unit test to check for memory leaks
+  stdout_res, stderr_res = run_sub_process(['valgrind', '--tool=memcheck', '--leak-check=full', './main'])
+
+  print stderr_res
+
+  if stderr_res.find("ERROR SUMMARY: 0 errors") == -1:
+    print "unit test + valigrind check...failed"
+  else:
+    print "unit test + valigrind check...passed"
+
+if __name__ == "__main__":
+  main()
--- main.cpp
+++ main.cpp
@@ -1,59 +1,14 @@
 #include "log_container.h"
 #include "log_container_iterator.h"
-#include <iostream>
-#include <string>
-#include <cassert>
 
 int main() {
     // your code goes here
-
-    std::string out; // used for unit testing
 
     log_container log("system_log", 520);
     log.append(string_record("hello_how_are_you"));
     log.append(string_record("_great"));
     log.append(string_record("_amazing"));
     log.append(string_record("_yes"));
-
-    assert(log.get_size() == 4);
-
-    out.clear();
-
-    for (log_container_iterator itr = log.begin(); itr != log.end(); itr++) {
-        out += *itr;
-    }
-
-    assert(out == "hello_how_are_you_great_amazing_yes");
-
-    log.truncate(0);
-    assert(log.get_size() == 3);
-
-    out.clear();
-    for (log_container_iterator itr = log.begin(); itr != log.end(); itr++) {
-        out += *itr;
-    }
-
-    assert(out == "_great_amazing_yes");
-
-    log.append(string_record("_wonderful"));
-
-    assert(log.get_size() == 4);
-
-    out.clear();
-    for (log_container_iterator itr = log.begin(); itr != log.end(); itr++) {
-        out += *itr;
-    }
-
-    assert(out == "_great_amazing_yes_wonderful");
-
-    return 0;
-
-    // empty log store
-    uint64_t size = log.get_position();
-    log.truncate(size-1);
-
-    assert(log.get_size() == 0);
-    assert(log.check_internal_data() == true);
 
     return 0;
 }
