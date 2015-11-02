a4fe0d2951170d75b004b04ce263f8808717a5bc
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-11-02 14:23:45 +0100

    added multithreading test

--- main_test.cpp
+++ main_test.cpp
@@ -1,10 +1,105 @@
 #include "log_container.h"
 #include "log_container_iterator.h"
+#include "util.h"
 #include <iostream>
 #include <string>
 #include <cassert>
+#include <thread>
+#include <chrono>
+#include <sstream>
+#include <mutex>
 
-int main() {
+class thread_test {
+  private:
+    log_container *log_;
+    std::mutex lock_;
+    std::vector<std::vector<std::string>> out;
+
+  public:
+    thread_test(log_container *log): log_(log) {}
+
+    void writer_thread() {
+        for (unsigned int i = 0; i < 100; i++) {
+            std::stringstream ss;
+            ss << i;
+            log_->append(string_record("hello_world: " + ss.str()));
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        }
+    }
+
+    void read_thread() {
+        std::vector<std::string> records;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+        for (log_container_iterator itr = log_->begin(); itr != log_->end(); itr++) {
+            records.push_back(*itr);
+        }
+
+        std::lock_guard<std::mutex> lock(lock_);
+        out.push_back(records);
+    }
+
+    std::vector<std::vector<std::string>> get_out() const {
+        return out;
+    }
+};
+
+bool check_read_data(std::vector<std::vector<std::string>> &&out) {
+    std::string pattern = "hello_world: ";
+
+    for (auto &vec : out) {
+        int64_t prev_count = -1;
+
+        for (auto &e : vec) {
+            size_t pos = e.find(pattern);
+            if (pos == std::string::npos) {
+                return false;
+            }
+
+            pos += pattern.size();
+            std::string counter = e.substr(pos, e.size() - pos);
+            int64_t count = string_to_int(counter);
+
+            if (count <= prev_count) {
+                return false;
+            }
+
+            prev_count = count;
+        }
+    }
+
+    return true;
+}
+
+void multithreading_test() {
+    std::vector<std::thread> reader_pool;
+
+    log_container log("system_log", 520);
+    thread_test t(&log);
+
+    std::thread writer(&thread_test::writer_thread, &t);
+
+    for (unsigned int i = 0; i < 10; i++) {
+        reader_pool.push_back(std::thread(&thread_test::read_thread, &t));
+    }
+
+    writer.join();
+    for (auto& thread : reader_pool) {
+        thread.join();
+    }
+
+    assert(check_read_data(t.get_out()) == true);
+
+    // empty log store
+    uint64_t pos = log.get_position();
+    log.truncate(pos-1);
+
+    assert(log.get_size() == 0);
+}
+
+void basic_test() {
     std::string out;
 
     log_container log("system_log", 520);
@@ -49,6 +144,12 @@ int main() {
     log.truncate(pos-1);
 
     assert(log.get_size() == 0);
+}
+
+int main() {
+
+    basic_test();
+    multithreading_test();
 
     return 0;
 }
