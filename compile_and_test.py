9b3786109b0b9c9c2b4fc67b865d1b12e1f7d6ce
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-11-09 13:10:02 +0100

    added clang static analyzer check

--- compile_and_test.py
+++ compile_and_test.py
@@ -18,6 +18,17 @@ def remove_files(fileext):
     os.remove(f)
 
 def main():
+  # call clang static analyzer
+  run_sub_process(['rm', '-rf', 'report'])
+  run_sub_process(['mkdir', '-p', 'report'])
+  # could also use subprocess.check_call and it'll raise subprocess.CalledProcessError if it's != 0
+  stdout_res, stderr_res = run_sub_process(['/usr/local/opt/llvm/share/clang/tools/scan-build/scan-build', '--use-analyzer=/usr/local/opt/llvm/bin/clang', '--view', '-o', 'report' ,'make'])
+
+  if stdout_res.find("No bugs found") == -1:
+    print "clang static analyzer check...failed"
+  else:
+    print "clang static analyzer check...passed"
+
   # compile unit test
   remove_files("*.config")
   remove_files("*.data")
@@ -26,7 +37,7 @@ def main():
 
   # run unit test
   stdout_res, stderr_res = run_sub_process(['./main'])
-  
+
   if stderr_res.find("Assertion") == -1:
     print "unit test...passed"
   else:
